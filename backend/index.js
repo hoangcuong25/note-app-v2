eddfa42924ea44975053eb61480e75a954316d50
hoang cuong <vancuong25112003@gmail.com>
2024-10-21 15:49:16 +0700

    init

---
+++ backend/index.js
@@ -0,0 +1,203 @@
+require("dotenv").config()
+
+const mongoose = require("mongoose")
+const config = require("./config.json")
+const bcrypt = require("bcrypt")
+const express = require("express")
+const cors = require("cors")
+
+const jwt = require("jsonwebtoken")
+
+const User = require("./models/user.model")
+const Note = require("./models/note.model")
+const { authenticateToken } = require("./utilities")
+
+mongoose.connect(config.connectString)
+
+const app = express()
+app.use(express.json())
+app.use(cors({ origin: "*" }))
+
+// test api
+app.get("/hello", async (req, res) => {
+    return res.status(200).json({ message: "hello hihi" })
+})
+
+// login 
+app.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        if (!email || !password) {
+            return res.status(400).json({ error: true, message: "All field are required" })
+        }
+
+        const user = await User.findOne({ email: email })
+
+        if (!user) {
+            return res.status(400).json({ error: true, message: "User not found" })
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+
+        if (!isPasswordValid) {
+            return res.status(400).json({ error: true, message: "Wrong password!" })
+        }
+
+        const accessToken = jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN_SECERT, { expiresIn: "72h" })
+
+        return res.status(200).json({
+            error: false,
+            user: user,
+            accessToken
+        })
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error })
+    }
+})
+
+// create account
+app.post("/create-account", async (req, res) => {
+    try {
+        const { name, email, password } = req.body
+
+        if (!name || !email || !password) {
+            return res.status(400).json({ error: true, message: "All field are required" })
+        }
+
+        const isUser = await User.findOne({ email: email })
+
+        if (isUser) {
+            return res.status(400).json({ error: true, message: "User already exist" })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        await user.save()
+
+        const accessToken = jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN_SECERT, { expiresIn: "72h" })
+
+        return res.status(200).json({
+            error: false,
+            user: user,
+            accessToken,
+            message: "Register Successfully"
+        })
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error })
+    }
+})
+
+// get user info
+app.get("/get-user", authenticateToken, async (req, res) => {
+    try {
+        const { userId } = req.user
+
+        const user = await User.findOne({ _id: userId })
+
+        if (!user) {
+            return res.status(400).json({ error: true, message: "User not found" })
+        }
+
+        return res.status(200).json({ error: false, user: user })
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error })
+    }
+})
+
+// add note
+app.post("/add-note", authenticateToken, async (req, res) => {
+    const { title, content, tags } = req.body
+    const { userId } = req.user
+
+    if (!title || !content) {
+        return res.status(400).json({
+            error: true,
+            message: "All fields are required"
+        })
+    }
+
+    try {
+        const note = new Note({
+            title,
+            content,
+            tags: tags || [],
+            userId: userId
+        })
+
+        await note.save();
+
+        return res.status(201).json({
+            error: false,
+            note: note,
+            message: "Add Note Successfully"
+        })
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error })
+    }
+})
+
+// update note
+app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
+    try {
+        const { noteId } = req.params
+        const { title, content } = req.body
+        const { userId } = req.user
+
+        const note = await Note.findOne({ _id: noteId, userId: userId })
+
+        if (!title && !content) {
+            return res.status(400).json({ error: true, message: "Nothing change" })
+        }
+
+        if (title) note.title = title
+        if (content) note.content = content
+
+        await note.save()
+
+        return res.status(200).json({ error: false, message: "Updated Successfully", note })
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error })
+    }
+})
+
+// delete note
+app.delete("/delete-note/:noteId", authenticateToken, async (req, res) => {
+    try {
+        const { noteId } = req.params
+        const { userId } = req.user;
+
+        await Note.deleteOne({ userId: userId, _id: noteId });
+
+        return res.status(200).json({ error: false, message: "Deleted Successfully" });
+    } catch (error) {
+        return res.status(500).json({ message: "Server error", error: error.message });
+    }
+});
+
+// get all notes
+app.get("/get-all-notes", authenticateToken, async (req, res) => {
+    try {
+        const { userId } = req.user
+
+        const notes = await Note.find({ userId: userId })
+
+        return res.status(200).json({ error: false, message: "Get all notes Succsessfully", notes })
+    } catch (error) {
+        return res.status(500).json({ message: "Server error", error: error.message });
+    }
+})
+
+
+
+app.listen(8000)
+module.exports = app
---
+++ backend/models/user.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    name: {type: String, require: true},
+    email: {type: String, require: true, unique: true},
+    password: {type: String, require: true},
+    createdOn: {type: String, default: Date.now} 
+})
+
+module.exports = mongoose.model("User", userSchema)
